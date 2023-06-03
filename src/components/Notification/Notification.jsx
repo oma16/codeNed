8fddafb9996dbb7fb64df9710c48ff1a1d5ac64a
Mariam <titioba95@gmail.com>
2023-06-03 23:08:26 +0100

    created the folders and components

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./notification.css";
+import Card from '../card/Card';
+
+const Notification = () => {
+  return (
+    <div>
+      <h1 className='notify'>NOTIFICATION</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default Notification
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./card.css";
+
+
+const Card = () => {
+  return (
+    <div>
+      card
+    </div>
+  )
+}
+
+export default Card
